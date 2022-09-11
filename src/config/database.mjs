c5e4cf86ffb05e7a1b3a6cfa2141c72c3290b0ce
Chipskein <abfn0905@gmail.com>
2022-09-11 16:15:41 -0300

    add vitest

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -1,7 +1,9 @@
 import { config } from 'dotenv';
 config();
+import { MongoClient } from 'mongodb'
 export class Database{
     constructor(CONNECTION_STR=process.env.MONGO_CONNECTION){
         this.CONNECTION_STR=CONNECTION_STR;
+        this.client=MongoClient(CONNECTION_STR);
     }
 }
