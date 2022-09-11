5608fd42df899d405297060eb0a322a9652e6809
Chipskein <abfn0905@gmail.com>
2022-09-11 16:53:08 -0300

    .

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 config();
 import { MongoClient } from 'mongodb'
 export class Database{
+    database_name="node-mongodb-weather-api";
+    collections=["weathers"];
     constructor(CONNECTION_STR=process.env.MONGO_CONNECTION){
         this.CONNECTION_STR=CONNECTION_STR;
         this.client=MongoClient(CONNECTION_STR);
